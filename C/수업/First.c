99979148443a3d780f9575041e2261e2c1c00db0
Dong-geon24 <rkdtjtneh@kyonggi.ac.kr>
2024-08-16 09:42:48 +0900

    [backip] ~24.8.16

--- C/수업/First.c
+++ C/수업/First.c
@@ -1,10 +1,18 @@
-
 #include <stdio.h>
+#include <math.h>
 
-int main(void)
-{
-    printf("%zu\n",sizeof(int));
-    printf("%zu\n",sizeof(long));
+double calculateSqrt(int *num) {
+    return sqrt(*num);
+}
+
+int main() {
+    int num;
+    printf("정수를 입력하세요: ");
+    scanf("%d", &num);
+
+    double result = calculateSqrt(&num);
+
+    printf("제곱근: %lf\n", result);
+
     return 0;
 }
- 
